4fbc29aa2f308725606bb6b67eae6b103f244b39
Ruhsuzbey <91276875+Ruhsuzbey@users.noreply.github.com>
2021-10-22 20:28:56 +0500

    Add files via upload

---
+++ plugins/tr.py
@@ -0,0 +1,7 @@
+import os
+from config import ASSISTANT_NAME
+
+HELP_TEXT =f"**SETTINGS** \n\n`1) Bu Botu Grubunuza Ekleyin ve Yönetici Yapın ✅ ` \n2) **Add** @{ASSISTANT_NAME} **Grubunuza** \n\n**COMMANDS** \n\n- `/stream` **: Vc'de Yayınlamak İstediğiniz Videoyu veya Dosyayı Yanıtlayın ✅ ** \n- `/stopstream`  **: Akışı durdurma**"
+START_TEXT = '👋 `Hey Benim Adım` **Kafkef** \n\n`Arkadaşlarınızla Grup Video Oynatıcının Sinematik Görünümünün Keyfini Çıkarın ❤️😊`'
+ABOUT_TEXT = '**👩‍💻 INFORMATION** \n\n`🤖 Bu bot, 🌹'den çeşitli yöntemler kullanılarak telegram grubu görüntülü sohbetlerinde video akışı için oluşturulmuştur.` \n\n`💡 PyTg, Telegram Grup Çağrıları için Async istemci API'sini çağırır ve Pyrogram, Kullanıcılar ve Botlar için Saf Python'da MTProto API İstemci Kütüphanesi ve Çerçevesi telgrafını çağırır.` \n\n**This bot licensed under GNU-GPL 3.0 License**'
+DEVS_TEXT =f"**CREDIT VC VIDEO OYNATıCı** \n\n`Burada Bazı Geliştiriciler Yapımında Yardımcı Oluyor` @{ASSISTANT_NAME} `Bot.`"
